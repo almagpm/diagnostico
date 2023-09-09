50b1fe44d2151cddecd09568fd06a75917e2bd9a
Alma P <20030274@itcelaya.edu.mx>
2023-09-09 15:42:03 -0600

    actualizacion

---
+++ controllers/sistema.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Clase principal del sistema.
+ *
+ * @autor 2023 Escribe tu nombre
+ */
+session_start();
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+
+require_once(__DIR__.'/../config.php');
+require_once(__DIR__.'/../configpsql.php');
+class Sistema
+{
+   var $db = null;
+   var $db2 = null;
+   /**
+    * Conexión a la base de datos
+    *
+    * @return PDOObject en $this->db
+    * @param del archivo de configuracion config.php
+    */
+   public function db()
+   {
+      $dsn = DBDRIVER . ':host=' . DBHOST . ';dbname=' . DBNAME . ';port=' . DBPORT;
+      $this->db = new PDO($dsn, DBUSER, DBPASS);
+   }
+  
+
+   /**
+    * Imprime un mensaje usando alerts de bootstrap
+    *
+    * @param $color el color del alert
+    *        $msg el mesaje a imprimir
+    */
+   public function flash($color, $msg)
+   {
+      include('views/flash.php');
+   }
+
+}
+$sistema = new sistema;
+?>
